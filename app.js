9a19a3b38a2c3950f16fcce4d202604af321d2ee
Akumaw <dev@akumaw.com>
2022-05-30 10:22:44 +0200

    js

---
+++ app.js
@@ -0,0 +1,21 @@
+let numONe, numTwo, SignOp ;
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    let clickElement = document.querySelectorAll('.number');
+    console.log(clickElement);
+
+    
+
+    
+
+    
+
+
+
+
+
+    
+});
